f31863ae4de73f963fffbd045be5b19890d4de34
Ujjwal Ojha <bring2uo@gmail.com>
2014-04-20 20:30:15 +0545

    Added tests for background test

---
+++ tests/HtImgModuleTest/Imagine/Filter/BackgroundTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace HtImgModule\Imagine\Filter;
+use HtImgModule\Imagine\Filter\Background;
+use Imagine\Gd\Imagine;
+
+class BackgroundTest extends \PHPUnit_Framework_TestCase
+{
+    public function testFilter()
+    {
+        $imagine = new Imagine();
+        $filter = new Background($imagine);
+        $archos = $imagine->open('resources/Archos.jpg');
+        $newImage = $filter->apply($archos);
+        $newImage->save('resources/background1.jpg');
+        $filter = new Background($imagine, [1050, 1060], 'red');
+        $newImage = $filter->apply($archos);
+        $newImage->save('resources/background2.jpg');
+        $this->assertFileExists('resources/background1.jpg');
+        $this->assertFileExists('resources/background2.jpg');
+        unlink('resources/background1.jpg');
+        unlink('resources/background2.jpg');
+    }
+}
