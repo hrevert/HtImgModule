a4c9d1bad174df2a0937b002599f15922757b9e3
Ujjwal Ojha <bring2uo@gmail.com>
2014-05-15 19:24:51 +0545

    Update tests for CacheManager

--- tests/HtImgModuleTest/Service/CacheManagerTest.php
+++ tests/HtImgModuleTest/Service/CacheManagerTest.php
@@ -7,21 +7,21 @@ use Imagine\Gd\Imagine;
 
 class CacheManagerTest extends \PHPUnit_Framework_TestCase
 {
-    protected $cacheManager;
-
-    public function setUp()
-    {
-        $this->cacheManager = new CacheManager(new ModuleOptions);
-    }
-
+    /**
+     * @covers HtImgModule\Service\CacheManager::getCacheUrl
+     */
     public function testGetCacheUrl()
     {
         $options = new ModuleOptions;
         $cacheManager = new CacheManager($options);
         $options->setCachePath(RESOURCES_DIR);
         $this->assertEquals(RESOURCES_DIR . '/hello/world', $cacheManager->getCacheUrl('world', 'hello'));
+        $this->assertEquals(RESOURCES_DIR . '/hello/world.jpg', $cacheManager->getCacheUrl('world', 'hello', RESOURCES_DIR . '/Archos.jpg'));
     }
 
+    /**
+     * @covers HtImgModule\Service\CacheManager::getCachePath
+     */
     public function testGetCachePath()
     {
         $options = new ModuleOptions;
@@ -32,6 +32,9 @@ class CacheManagerTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(RESOURCES_DIR . '/hello/Archos.jpg.jpg', $cacheManager->getCacheUrl('Archos.jpg', 'hello', RESOURCES_DIR . '/Archos.jpg'));
     }
 
+    /**
+     * @covers HtImgModule\Service\CacheManager::createCache
+     */
     public function testCreateCache()
     {
         $options = new ModuleOptions;
@@ -40,9 +43,37 @@ class CacheManagerTest extends \PHPUnit_Framework_TestCase
         $image = $imagine->open(RESOURCES_DIR . '/Archos.jpg');
         $options->setWebRoot(RESOURCES_DIR);
         $options->setCachePath('.');
-        $cacheManager->createCache('archos-crop.jpg', 'crop', $image);
-        $cacheFile = RESOURCES_DIR . '/crop/archos-crop.jpg';
+        $cacheManager->createCache('awesome/archos-crop.jpg', 'crop', $image);
+        $cacheFile = RESOURCES_DIR . '/crop/awesome/archos-crop.jpg';
         $this->assertTrue(is_readable($cacheFile));
-        unlink($cacheFile);
+        
+        return $cacheManager;
+    }
+
+    /**
+     * @covers HtImgModule\Service\CacheManager::deleteCache
+     * @depends testCreateCache
+     */
+    public function testDeleteCache(CacheManager $cacheManager)
+    {
+        $cacheManager->deleteCache('awesome/archos-crop.jpg', 'crop');
+        $this->assertFalse(is_readable(RESOURCES_DIR . '/crop/awesome/archos-crop.jpg'));
+    }
+
+    /**
+     * @covers HtImgModule\Service\CacheManager::cacheExists
+     */
+    public function testCacheExists()
+    {
+        $options = new ModuleOptions;
+        $cacheManager = new CacheManager($options);
+        $imagine = new Imagine;
+        $image = $imagine->open(RESOURCES_DIR . '/Archos.jpg');
+        $options->setWebRoot(RESOURCES_DIR);
+        $options->setCachePath('.');
+        $cacheManager->createCache('awesome/archos-crop.jpg', 'crop', $image);
+        $cacheFile = RESOURCES_DIR . '/crop/awesome/archos-crop.jpg';
+        $this->assertTrue($cacheManager->cacheExists('awesome/archos-crop.jpg', 'crop'));
+        $this->assertFalse($cacheManager->cacheExists('awesome/random-crop.jpg', 'crop'));
     }
 }
