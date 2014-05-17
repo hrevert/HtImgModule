372e9f089f5c71a2dacf38aa3b37e2e0a7cb569f
Ujjwal Ojha <bring2uo@gmail.com>
2014-05-17 20:08:00 +0545

    Added callback image loader

---
+++ src/Imagine/Loader/CallbackLoader.php
@@ -0,0 +1,28 @@
+<?php
+namespace HtImgModule\Imagine\Loader;
+
+class CallbackLoader implements LoaderInterface
+{
+    /**
+     * @var callable
+     */
+    protected $callback;
+
+    /**
+     * Constructor
+     *
+     * @param callable $callback
+     */    
+    public function __construct(callable $callback)
+    {
+        $this->callback = $callback;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function load($path)
+    {
+        return call_user_func($this->callback, $path);
+    }
+}
