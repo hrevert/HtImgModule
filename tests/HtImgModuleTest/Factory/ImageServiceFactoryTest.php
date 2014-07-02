8a6937e5de2529181392535c8b2765a342818427
Ujjwal Ojha <bring2uo@gmail.com>
2014-07-02 19:15:30 +0545

    Update some tests

--- tests/HtImgModuleTest/Factory/ImageServiceFactoryTest.php
+++ tests/HtImgModuleTest/Factory/ImageServiceFactoryTest.php
@@ -13,9 +13,10 @@ class ImageServiceFactoryTest extends \PHPUnit_Framework_TestCase
         $serviceManager = new ServiceManager();
         $serviceManager->setService('HtImg\ModuleOptions', new ModuleOptions);
         $serviceManager->setService('HtImg\Imagine', $this->getMock('Imagine\Image\ImagineInterface'));
-        $serviceManager->setService('HtImg\RelativePathResolver', $this->getMock('Zend\View\Resolver\ResolverInterface'));
         $serviceManager->setService('HtImgModule\Imagine\Filter\FilterManager', $this->getMock('HtImgModule\Imagine\Filter\FilterManagerInterface'));
         $serviceManager->setService('HtImgModule\Service\CacheManager', $this->getMock('HtImgModule\Service\CacheManagerInterface'));
+        $serviceManager->setService('HtImgModule\Imagine\Loader\LoaderManager', $this->getMock('HtImgModule\Imagine\Loader\LoaderManagerInterface'));
+
 
         $factory = new ImageServiceFactory();
         $this->assertInstanceOf('HtImgModule\Service\ImageService', $factory->createService($serviceManager));
