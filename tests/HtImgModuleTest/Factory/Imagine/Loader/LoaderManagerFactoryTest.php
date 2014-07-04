69ffb4102c166170132c1993b24a738d2bfb257c
Ujjwal Ojha <bring2uo@gmail.com>
2014-07-04 20:34:03 +0545

    Update tests for factory
    Add tests for  HtImgModule\Factory\Imagine\Loader\LoaderManagerFactory

--- tests/HtImgModuleTest/Factory/Imagine/Loader/LoaderManagerFactoryTest.php
+++ tests/HtImgModuleTest/Factory/Imagine/Loader/LoaderManagerFactoryTest.php
@@ -1,10 +1,27 @@
 <?php
 namespace HtImgModuleTest\Factory\Imagine\Loader;
 
+use HtImgModule\Factory\Imagine\Loader\LoaderManagerFactory;
+use Zend\ServiceManager\ServiceManager;
+
 class LoaderManagerFactoryTest extends \PHPUnit_Framework_TestCase
 {
     public function testFactory()
     {
+        $serviceManager     = new ServiceManager();
+        $filterManager      = $this->getMock('HtImgModule\Imagine\Filter\FilterManagerInterface');
+        $imageLoaders       = $this->getMock('Zend\ServiceManager\ServiceLocatorInterface');
+        $options            = $this->getMock('HtImgModule\Options\ModuleOptions');
+        $mimeTypeGuesser    = $this->getMockBuilder('HtImgModule\Binary\MimeTypeGuesser')
+            ->disableOriginalConstructor()
+            ->getMock();
 
+        $serviceManager->setService('HtImgModule\Imagine\Filter\FilterManager', $filterManager);
+        $serviceManager->setService('HtImgModule\Imagine\Loader\LoaderPluginManager', $imageLoaders);
+        $serviceManager->setService('HtImgModule\Binary\MimeTypeGuesser', $mimeTypeGuesser);
+        $serviceManager->setService('HtImg\ModuleOptions', $options);
+
+        $factory = new LoaderManagerFactory;
+        $this->assertInstanceOf('HtImgModule\Imagine\Loader\LoaderManager', $factory->createService($serviceManager));
     }
 }
